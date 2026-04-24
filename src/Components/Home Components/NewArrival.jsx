59d89aea2c28e546653cd9f33a7a48dbf0730341
HarshalTarwale <tarwaleh@gmail.com>
2026-04-25 01:13:38 +0530

    Fix New Arrival image paths

--- src/Components/Home Components/NewArrival.jsx
+++ src/Components/Home Components/NewArrival.jsx
@@ -37,22 +37,22 @@ const NewArrival = () => {
             // breakpoints={{ 768: { slidesPerView: 2 }, 1024: { slidesPerView: 3 } }} - Responsive slides
           >
             <SwiperSlide>
-              <NewCard imageSrc="Home/Arrival images/OMEGA50 anniversary - Charles Helleu 1.png" />
+              <NewCard imageSrc="Home/Arrival images/newarrival-1.png" />
             </SwiperSlide>
             <SwiperSlide>
-              <NewCard imageSrc="Home/Arrival images/NASA Took OMEGA's Speedmaster to the Moon; Swatch Took it Across the Galaxy 1.png" />
+              <NewCard imageSrc="Home/Arrival images/newarrival-2.png" />
             </SwiperSlide>
             <SwiperSlide>
-              <NewCard imageSrc="public\Home\Arrival images\OMEGA50 anniversary - Charles Helleu 1.png" />
+              <NewCard imageSrc="Home/Arrival images/newarrival-3.png" />
             </SwiperSlide>
             <SwiperSlide>
-              <NewCard imageSrc="Home/Arrival images/Jewelry — Dylan Griffin Photography 1.png" />
+              <NewCard imageSrc="Home/Arrival images/newarrival-4.png" />
             </SwiperSlide>
             <SwiperSlide>
-              <NewCard imageSrc="Home/Arrival images/download (3) 1.png" />
+              <NewCard imageSrc="Home/Arrival images/newarrival-5.png" />
             </SwiperSlide>
             <SwiperSlide>
-              <NewCard imageSrc="Home/Arrival images/download (2) 1.png" />
+              <NewCard imageSrc="Home/Arrival images/newarrival-6.png" />
             </SwiperSlide>
           </Swiper>
         </div>
