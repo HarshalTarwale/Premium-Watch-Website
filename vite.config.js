f48af286d3e92ab1f706e68bca18c168ed50242e
HarshalTarwale <tarwaleh@gmail.com>
2026-05-09 13:18:16 +0530

    build for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Titan-Website/',
+  base: '/Premium-Watch-Website/',
+  build: {
+    outDir: 'docs',
+  },
 })
